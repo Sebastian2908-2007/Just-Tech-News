a56f12c3ee717193ce753caf5d7342131f6ac0b5
sebastian <sebastian bowen>
2021-04-14 22:06:23 -0600

    comments now enabled main develop merges commit

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Post, User, Vote} = require('../../models');
+const {Post, User, Vote, Comment} = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all posts
@@ -11,10 +11,18 @@ router.get('/', (req, res) => {
     ],
       order: [['created_at', 'DESC']],
       include: [
+          {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+          },
         {
           model: User,
           attributes: ['username']
-        }
+        },
       ]
     })
       .then(dbPostData => res.json(dbPostData))
